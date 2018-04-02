7326f4dc9555d00ffc82c7b9454c6e0058e109ed
Neoshaman <37513426+Neoshaman@users.noreply.github.com>
2018-04-02 20:50:17 -0300

    Update gameIdea.js

--- gameIdea.js
+++ gameIdea.js
@@ -232,12 +232,13 @@ document.getElementById("test").innerHTML =
 "<br> <font color = \"red\">World type: </font><font color = \"gray\">size, tone, genre</font><br>" + worldtype() +
 "<br> <font color = \"red\">Concept: </font><br>" + concept() +
 "<br>" +
+"<br> <font color = \"red\">Theme: <br> </font>" + balance() +
+"<br> <font color = \"red\">Set up : <br> </font>" + setupResult() +
 "<br> <font color = \"red\">Goal: <br></font>" + resolution() + //Goal -> resolution
+"<br>" +
 "<br> <font color = \"red\">Attitude: <br></font>" + attitude() +
 "<br> <font color = \"red\">Action: <br></font>" + action() +
 "<br> <font color = \"red\">Event: <br></font>" + events() +
-"<br> <font color = \"red\">Theme: <br> </font>" + balance() +
-"<br> <font color = \"red\">Set up : <br> </font>" + setupResult() +
 "<br>"
 ;
 }
