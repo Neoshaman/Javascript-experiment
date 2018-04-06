0bcf3c2f21dda6eb988f9be12ef0aa29009b51db
Neoshaman <37513426+Neoshaman@users.noreply.github.com>
2018-04-05 21:54:12 -0300

    Update gameIdea.js

--- gameIdea.js
+++ gameIdea.js
@@ -250,7 +250,7 @@ var elem2 = document.getElementsByTagName("p");
 elem1.innerHTML = " something1";
 elem2[0].innerHTML = " something2";
 GameDesign();
-setInterval(GameDesign,10000);
+//setInterval(GameDesign,10000);
 
 //todo
 //character attribute generator
